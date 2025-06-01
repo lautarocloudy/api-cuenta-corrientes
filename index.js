1823d77dd644384b3433e98672fe8a77ecb0c186
lautaro <lautarorondan96@gmail.com>
2025-06-01 17:32:39 -0300

    fix

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ app.use('/api/balance', require('./Routes/balance'));
 
 // Puerto desde .env o por defecto 4000
 const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// });
 
-
+module.exports = app;
 db.query('SELECT NOW()')
   .then(res => console.log('Conexión OK:', res.rows[0]))
   .catch(err => console.error('Error de conexión:', err));
